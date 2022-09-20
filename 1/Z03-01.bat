ccee806b49d6160910f32fccb79f8ad93e5d732d
Kate <2003katy2003@mail.ru>
2022-09-20 15:36:46 +0300

    .

---
+++ 1/Z03-01.bat
@@ -0,0 +1,6 @@
+@echo off
+@chcp 1251
+echo -- текущий пользователь: %username%
+echo -- текущие дата и время: %date%, %time%
+echo -- имя компьютера: %computername%
+pause
---
+++ 1/Z03-02.bat
@@ -0,0 +1,6 @@
+@echo off
+@chcp 1251
+echo -- имя этого bat-файла: %0
+echo -- этот bat-файл создан: %~t0
+echo -- путь bat-файла: %~f0
+pause
